32cf969fb043290629cdb27e68021bb01638316f
Bas van Driel <contact@basvandriel.nl>
2024-11-21 11:10:37 +0100

    remove todo

--- app/jwt_validator.py
+++ app/jwt_validator.py
@@ -16,7 +16,6 @@ class JWTPayload:
     f9_certificate: str
 
 
-# TODO this is a challenge validator
 class UZIJWTValidator:
     def _decode_jwt(self, token: str):
         # When this is implemented in production, verify the token key, audience and issuer.
